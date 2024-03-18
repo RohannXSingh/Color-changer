29311a227f37d9bfb9cfaa9bcb69b6d31e64c6a5
RohannXSingh <155614503+RohannXSingh@users.noreply.github.com>
2024-03-18 15:47:17 +0530

    JavaScriptProject

---
+++ JavaScriptproject/project.js
@@ -0,0 +1,40 @@
+
+const colorBlack = document.querySelector('#color-black')
+colorBlack.addEventListener('click',function(colorChange){
+    document.body.style.backgroundColor = '#2b2b35'
+    const btn = document.querySelector('#btn')
+    btn.style.backgroundColor = '#2b2b35'
+    btn.style.color = '#fff'
+      
+});
+const colorPink = document.querySelector('#color-pink')
+colorPink.addEventListener('click',function(colorChange){
+    document.body.style.backgroundColor = '#ef7d8f'
+    const btn = document.querySelector('#btn')
+    btn.style.backgroundColor = '#ef7d8f'
+    btn.style.color = '#fff'
+});
+const colorRed= document.querySelector('#color-red')
+colorRed.addEventListener('click',function(colorChange){
+    document.body.style.backgroundColor = '#fe4218'
+    const btn = document.querySelector('#btn')
+    btn.style.backgroundColor = '#fe4218'
+    btn.style.color = '#fff'
+    
+});
+const colorGreen = document.querySelector('#color-green')
+colorGreen.addEventListener('click',function(colorChange){
+    document.body.style.backgroundColor = '#44e46c'
+    const btn = document.querySelector('#btn')
+    btn.style.backgroundColor = '#44e46c'
+    btn.style.color = '#fff'
+});
+const colorYellow = document.querySelector('#color-yellow')
+colorYellow.addEventListener('click',function(colorChange1){
+    document.body.style.backgroundColor = '#f6ed40'
+    const btn = document.querySelector('#btn')
+    btn.style.backgroundColor = '#f6ed40'
+    btn.style.color = '#fff'
+});
+
+
